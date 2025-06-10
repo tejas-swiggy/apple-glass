f99ef6c4a1a3971cd5fbe7d5200a31a06cefa231
Tejas Nishad <214769083+tejas-swiggy@users.noreply.github.com>
2025-06-10 11:32:13 +0530

    Backing up working version before adding magnetic effect and cursor scale

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,13 @@ import TextDistort from "./TextDistort";
 import { Environment } from "@react-three/drei";
 
 export default function App() {
-  return (
-    <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
-      <ambientLight intensity={1} />
-      <Environment preset="city" />
-      <Cursor />
-      <TextDistort />
-    </Canvas>
-  );
+    return (
+        <Canvas camera={{ position: [0, 0, 5], fov: 50 }}>
+            <ambientLight intensity={1} />
+            <Environment preset="city" />
+            <Cursor/>
+            <TextDistort/>
+        </Canvas>
+    );
 }
 
--- src/TextDistort.jsx
+++ src/TextDistort.jsx
@@ -1,16 +1,23 @@
 // src/TextDistort.jsx
 import { Text } from "@react-three/drei";
+import Inter from "./assets/Inter.ttf";
 
-export default function TextDistort() {
-  return (
-    <Text
-      position={[0, 0, 0]}
-      fontSize={0.5}
-      color="#ffffff"
-      anchorX="center"
-      anchorY="middle"
-    >
-      Hover Me
-    </Text>
-  );
+export default function TextDistort({ onHoverChange }) {
+
+    return (
+        <Text
+            position={[0, 0, 0]}
+            font={Inter}
+            fontSize={0.5}
+            letterSpacing={-0.07} // -2% spacing
+            color="#ffffff"
+            anchorX="center"
+            anchorY="middle"
+            sdfGlyphSize={512} // Higher for better detail, lower for performanceß
+            onPointerOver={() => onHoverChange(true)}
+            onPointerOut={() => onHoverChange(false)}
+        >
+            Hover Me
+        </Text>
+    );
 }
